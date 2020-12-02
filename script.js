05042c66a83c3790e3353d073a03cb2f017d0e67
Greg <gkodrucz@gmail.com>
2020-12-02 13:47:20 +0000

    create burger menu

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
 function _load(){
 
-  document.getElementById("root").innerHTML = "<h1>wőrld</h1>";
-  document.getElementById("root").innerHTML += "<h2>európa</h2>";
+  function clickEvent(){
+    //ez is mukodik, ha a css-ben le van kezelve a #nav #open elemeken a clicked class name
+    //document.getElementById("open").classList.toggle("clicked");
+    //document.getElementById("nav").classList.toggle("clicked");
 
-  document.getElementById("root").insertAdjacentHTML("beforeend", "<h3>wow,text</h3>");
+    document.querySelector("body").classList.toggle("clicked");
+  }
 
-  document.getElementById("root").insertAdjacentHTML("beforeend", `
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium recusandae iure voluptate et illo magni? Error, quod voluptas? Optio quas saepe, explicabo mollitia odit quod cumque placeat iste debitis.
-    </p>
-  `);
-
-  //Ezzel az egesz elemet kitoroljuk vagy felul irjuk, sajat magat is beleertve.
-  //document.getElementById("root").outerHTML = "";
-  
-  //Ezt sem erdemes hasznalni, a css-nek a css-ben a helye.
-  //document.getElementById("root").style.padding = "80px";
-
-  //Ezt nem erdemes hasznalni
-  //document.getElementById("root").className = "rootClass";
-  //document.getElementById("root").className += " welcomeClass";
-
-  document.getElementById("root").classList.add("rootClass");
-  document.getElementById("root").classList.add("welcomeClass");
-  document.getElementById("root").classList.add("tempClass");
-
-  document.getElementById("root").classList.remove("tempClass");  
-
+  document.getElementById("open").addEventListener("click", clickEvent);
   
 }
-
-//ez igy nem fog betoltodni.
-//document.getElementById("root").innerHTML = "<h1>wőrld</h1>";
-
-//igy sem erdemes csinalni
-//window.onload = _load();
-
 
 window.addEventListener("load", _load);
